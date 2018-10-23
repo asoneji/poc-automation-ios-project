82cfe70ba5082de000a0f28dcec1399798941276
Soneji, Ankit <Ankit_Soneji@intuit.com>
2018-10-23 16:17:28 -0700

    added simple functionality to testing app

--- SampleTestAutomationApp/SampleTestAutomationApp/ViewController.swift
+++ SampleTestAutomationApp/SampleTestAutomationApp/ViewController.swift
@@ -13,8 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        sampleTestAutomationLabel.accessibilityIdentifier = "sampleTestAutomationLabel"
+        sendMessageLabel.accessibilityIdentifier = "sendMessageLabel";
+        sendMessageTextField.accessibilityIdentifier = "sendMessageTextField";
+        receivedMessageLabel.accessibilityIdentifier = "receivedMessageLabel";
+        receivedMessageTextField.accessibilityIdentifier = "receivedMessageTextField";
+        
+        sendMessageButton.accessibilityIdentifier = "sendMessageButton";
+        clearMessageButton.accessibilityIdentifier = "clearMessageButton";
+        
+        receivedMessageTextField.isUserInteractionEnabled = false;
     }
 
+    @IBOutlet weak var sampleTestAutomationLabel: UILabel!
+    @IBOutlet weak var sendMessageLabel: UILabel!
+    @IBOutlet weak var sendMessageTextField: UITextField!
 
+    @IBOutlet weak var receivedMessageLabel: UILabel!
+    @IBOutlet weak var receivedMessageTextField: UITextField!
+
+    @IBOutlet weak var sendMessageButton: UIButton!
+    @IBOutlet weak var clearMessageButton: UIButton!
+    
+    @IBAction func sendMessageButtonAction(_ sender: UIButton) {
+        receivedMessageTextField.text = sendMessageTextField.text
+    }
+    
+    @IBAction func clearMessageButtonAction(_ sender: UIButton) {
+        sendMessageTextField.text = ""
+        receivedMessageTextField.text = ""
+    }
 }
 
