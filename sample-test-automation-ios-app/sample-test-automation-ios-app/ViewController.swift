f6c4c2062bb02d06f115ce0a042c96028edce948
Soneji, Ankit <Ankit_Soneji@intuit.com>
2018-10-23 12:19:00 -0700

    initial empty ios proejct

---
+++ sample-test-automation-ios-app/sample-test-automation-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample-test-automation-ios-app
+//
+//  Created by Soneji, Ankit on 10/23/18.
+//  Copyright © 2018 Soneji, Ankit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
